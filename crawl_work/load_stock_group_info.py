f5d3d946fa07f13939c6c5b26478aae3c38486ff
truegary <truegary@139.com>
2019-09-28 10:44:02 +0800

    create crawl_work dir name default init file

--- crawl_work/load_stock_group_info.py
+++ crawl_work/load_stock_group_info.py
@@ -4,3 +4,9 @@ import urllib.request
 import traceback
 import time
 
+url_group_index = "http://summary.jrj.com.cn/hybk/index.shtml?q=cn|bk|17&n=hqa&c=l&o=pl,d&p=1050"
+crawl_time_out = 5
+max_retry_times = 3
+crawl_sleep_time = 5
+page_coding = "GB2312"
+group_industry_flag = "popmenu1"
