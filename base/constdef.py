f704869f20c86ba301b2bbb70fb4f91ea704f176
truegary <truegary@139.com>
2019-11-24 16:15:48 +0800

    commit

--- base/constdef.py
+++ base/constdef.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-app_conf_dir = "/Volumes/macdisk2/garywork/python_work/superwave/conf/"
-code_file = "/Volumes/macdisk2/garywork/stock/stockdata/codes/fundcodelist.csv"
-stock_code_file = "/Volumes/macdisk2/garywork/stock/stockdata/codes/stockcodelist.csv"
-data_path = "/Volumes/macdisk2/garywork/stock/stockdata/backtesting/hisdata/"
-stock_code_in_categery = "/Volumes/macdisk2/garywork/stock/common_data/stock_in_type.csv"
-stock_month_file = "/Volumes/macdisk2/garywork/stock/stockdata/month/allmonth.csv"
+app_conf_dir = "/workdata/stockapp_config/"
+code_file = "/workdata/stockdata/codes/fundcodelist.csv"
+stock_code_file = "/workdata/stockdata/codes/stockcodelist.csv"
+data_path = "/workdata/stockdata/backtesting/hisdata/"
+stock_code_in_categery = "/workdata/common_data/stock_in_type.csv"
+stock_month_file = "/workdata/stockdata/month/allmonth.csv"
 
-report_path = "/Volumes/macdisk2/garywork/stock/stockdata/report/"
+report_path = "/workdata/stockdata/report/"
 
 s_date = "2001-12-01"
 e_date = "2099-12-31"
-fund_data_dir = "/Volumes/macdisk2/garywork/stock/stockdata/fund/"
+fund_data_dir = "/workdata/stockdata/fund/"
 
-work_reuslt_log_dir = "/Volumes/macdisk2/garywork/stock/stockdata/log/"
-work_temp_filr_dir = "/Volumes/macdisk2/garywork/stock/stockdata/tmp/"
+work_reuslt_log_dir = "/workdata/stockdata/log/"
+work_temp_filr_dir = "/workdata/stockdata/tmp/"
