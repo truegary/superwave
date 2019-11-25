ceb36df4384c521bc56f3c19e461c89aea0d6409
truegary <truegary@139.com>
2019-11-25 22:17:37 +0800

    commit

--- load_finance_data/run/crawlfundnewdata.py
+++ load_finance_data/run/crawlfundnewdata.py
@@ -8,6 +8,7 @@ from base import constdef
 from stockutil.dataloadutil.loadfundutil import load_fund_code
 from datacrawl.crawlfundwork import CrawlFundData
 from base.workutil import caltimediff
+from pandas.io.common import EmptyDataError
 
 out_dir = constdef.fund_data_dir
 s_date = "2001-12-01"
@@ -15,12 +16,33 @@ e_date = "2099-12-31"
 min_code_value = 0
 
 
+def save_to_disk(t_fileName, pd_data):
+    try_times = 0
+    iResult = 0
+
+    while try_times < 5:
+        try:
+            pd_data.to_csv(t_fileName, columns=pd_data.columns.values.tolist(), index=True)
+            iResult = 0
+            break
+        except OSError:
+            try_times = try_times + 1
+            print("Error to save to file,try times:%d (%s)" % (try_times, t_fileName))
+            iResult = 1
+
+    return iResult
+
+
 def crawl_new_data(code_str,crawlHandle,work_start_time):
     target_file_name = out_dir + code_str + ".csv"
+
     load_data = None
     if os.path.exists(target_file_name):
-        load_data = pd.read_csv(target_file_name, dtype=object, parse_dates=False, index_col=0)
-        load_data.sort_index(inplace=True)
+        try:
+            load_data = pd.read_csv(target_file_name, dtype=object, parse_dates=False, index_col=0)
+            load_data.sort_index(inplace=True)
+        except EmptyDataError:
+            load_data = None
 
     crawlHandle.init_work(code_str, s_date, e_date)
     fund_data = crawlHandle.do_crawl_new_data()
@@ -32,7 +54,7 @@ def crawl_new_data(code_str,crawlHandle,work_start_time):
         return
 
     if load_data is None:
-        fund_data.to_csv(target_file_name, columns=fund_data.columns.values.tolist(), index=True)
+        save_to_disk(target_file_name, fund_data)
         currtime = datetime.datetime.now()
         hour, min, sec = caltimediff(work_start_time, currtime)
         print("done for crawl %s,total count:%d,crawl new count:%d, spend times: %d hours,%d mins,%dsecs" % (code_str, len(fund_data), len(fund_data),hour, min, sec))
@@ -47,7 +69,8 @@ def crawl_new_data(code_str,crawlHandle,work_start_time):
         load_data.drop_duplicates('date', 'first', inplace=True)
 
         load_data.sort_index(inplace=True)
-        load_data.to_csv(target_file_name, columns=load_data.columns.values.tolist(), index=True)
+        save_to_disk(target_file_name, load_data)
+
         currtime = datetime.datetime.now()
         hour, min, sec = caltimediff(work_start_time, currtime)
         print("done for crawl %s,total count:%d,crawl new count:%d, spend times: %d hours,%d mins,%dsecs" % (code_str, len(load_data),len(fund_data),hour, min, sec))
